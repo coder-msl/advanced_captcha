17e7bd589033e475af9f4de20dc10f987f654cfc
Mateusz Lach <matis_l@tlen.pl>
2026-07-12 16:18:31 +0200

    modal added

--- src/script.js
+++ src/script.js
@@ -15,27 +15,29 @@ $(document).ready(function() {
     Promise.all([
         loadCss("https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css"),
         $.getScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js")
+        
     ]).then(function () {
-        console.log("Biblioteka gotowa do użycia");
+
+        $(`
+            <div id="ex1" class="modal">
+                <p>Thanks for clicking. That felt good.</p>
+                <a href="#" rel="modal:close">Close</a>
+            </div>
+        `)
+        .appendTo("body")
+        .modal();
+
+
+        $("#copyButton").on("click", function () {
+            navigator.clipboard.writeText("Hello World")
+                .then(() => {
+                    console.log("Skopiowano");
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        });
     });
 
-    $(`
-        <div id="ex1" class="modal">
-            <p>Thanks for clicking. That felt good.</p>
-            <a href="#" rel="modal:close">Close</a>
-        </div>
-    `)
-    .appendTo("body")
-    .modal();
 
-
-    $("#copyButton").on("click", function () {
-        navigator.clipboard.writeText("Hello World")
-            .then(() => {
-                console.log("Skopiowano");
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    });
 })
