899885ed05fcc9fd46a0813e33d34d01dfc28ae2
Mateusz Lach <matis_l@tlen.pl>
2026-07-12 16:15:52 +0200

    modal added

--- src/script.js
+++ src/script.js
@@ -1,3 +1,41 @@
+function loadCss(url) {
+    return new Promise(function(resolve, reject) {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = url;
+        link.onload = resolve;
+        link.onerror = reject;
+        document.head.appendChild(link);
+    });
+}
+
 $(document).ready(function() {
-    alert(2);
+
+
+    Promise.all([
+        loadCss("https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css"),
+        $.getScript("https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js")
+    ]).then(function () {
+        console.log("Biblioteka gotowa do użycia");
+    });
+
+    $(`
+        <div id="ex1" class="modal">
+            <p>Thanks for clicking. That felt good.</p>
+            <a href="#" rel="modal:close">Close</a>
+        </div>
+    `)
+    .appendTo("body")
+    .modal();
+
+
+    $("#copyButton").on("click", function () {
+        navigator.clipboard.writeText("Hello World")
+            .then(() => {
+                console.log("Skopiowano");
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    });
 })
